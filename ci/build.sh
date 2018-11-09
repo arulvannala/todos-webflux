19e26f1c5e91016a501fe9920411f64a55568239
Corbs <corbsmartin@pivotal.io>
2018-11-08 21:37:19 -0600

    testing ci

--- ci/build.sh
+++ ci/build.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 export MAVEN_OPTS="-Xms2g -Xmx2g"
+export VERSION=`cat source-code/version`
+echo "Building version $VERSION"
 cd source-code
-  ./mvnw clean package -Dmaven.test.skip=true
+  ./mvnw clean package -Dversion=${VERSION} -Dmaven.test.skip=true
 cd ..
 
-cp source-code/target/todos-webflux-1.0.0.SNAP.jar  build-output/.
+cp source-code/target/todos-webflux-${VERSION}.jar  build-output/.
