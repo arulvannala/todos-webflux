9386d15d5022091f5029eff4d548beef405ed74c
Dodd Pfeffer <dpfeffer@pivotal.io>
2019-08-18 16:04:31 -0400

    Updates CI scripts

--- ci/tasks/build-and-publish.sh
+++ ci/tasks/build-and-publish.sh
@@ -3,33 +3,9 @@
 export VERSION=`cat version/version`
 echo "Build version: ${VERSION}"
 
-echo "list of current directory contents"
-ls -ltr
-ls -ltr ci-scripts
-ls -ltr ci-scripts/ci
-ls -ltr ci-scripts/ci/tasks
+cd code-repo
 
-
-
-echo "Calling script to generate settings"
-cat ci-scripts/ci/tasks/generate-settings.sh
-
-cd ci-scripts/ci/tasks
-
-ls -ltr
-
-./generate-settings.sh
-
-[[ -z "${M2_HOME}" ]] && M2_HOME="${HOME}/.m2"
-[[ -z "${M2_CACHE}" ]] && M2_CACHE="${ROOT_FOLDER}/maven"
-
-echo "Generating symbolic links for caches"
-
-[[ -d "${M2_CACHE}" && ! -d "${M2_HOME}" ]] && ln -s "${M2_CACHE}" "${M2_HOME}"
-
-echo "Writing maven settings to [${M2_HOME}/settings.xml]"
-
-cat > "${M2_HOME}/settings.xml" <<EOF
+cat > "settings.xml" <<EOF
 
 <?xml version="1.0" encoding="UTF-8"?>
 <settings>
@@ -47,11 +23,10 @@ EOF
 echo "Settings xml written"
 
 
-cd ../../../code-repo
-
 echo "Running mvn deploy command"
 ./mvnw deploy \
     -DnewVersion=${VERSION} \
     -DskipTests \
     -Ddistribution.management.release.id="${M2_SETTINGS_REPO_ID}" \
-    -Ddistribution.management.release.url="${REPO_WITH_BINARIES_FOR_UPLOAD}"
+    -Ddistribution.management.release.url="${REPO_WITH_BINARIES_FOR_UPLOAD}" \
+    --settings settings.xml
