c2303af5b129be213b1899ffc4553bf88702273b
Dodd Pfeffer <dpfeffer@pivotal.io>
2019-08-18 12:44:48 -0400

    Removes unncessary files from branch

--- src/main/bash/smoke-tests.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-http :8010/ title="make bacon pancakes"
-http :8010/ title="make vegan bacon pancakes"
-http :8010/ title="eat pancakes"
-
-http PATCH :8010/1 completed=true
-http PATCH :8010/2 completed=true
-
--- src/main/bash/todos_api_limit_test.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-http :8010/todos/ title="one todo"
-http :8010/todos/ title="two todo"
-http :8010/todos/ title="three todo"
-http :8010/todos/ title="four todo"
-http :8010/todos/ title="five todo"
-http :8010/todos/ title="six todo"
