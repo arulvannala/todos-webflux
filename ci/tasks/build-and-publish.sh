0c905da7c72ae82cf3f2fc40d926b873d3da9283
Dodd Pfeffer <dpfeffer@pivotal.io>
2019-08-18 15:59:54 -0400

    Updates CI scripts

--- ci/tasks/build-and-publish.sh
+++ ci/tasks/build-and-publish.sh
@@ -16,7 +16,36 @@ cat ci-scripts/ci/tasks/generate-settings.sh
 
 cd ci-scripts/ci/tasks
 
+ls -ltr
+
 ./generate-settings.sh
+
+[[ -z "${M2_HOME}" ]] && M2_HOME="${HOME}/.m2"
+[[ -z "${M2_CACHE}" ]] && M2_CACHE="${ROOT_FOLDER}/maven"
+
+echo "Generating symbolic links for caches"
+
+[[ -d "${M2_CACHE}" && ! -d "${M2_HOME}" ]] && ln -s "${M2_CACHE}" "${M2_HOME}"
+
+echo "Writing maven settings to [${M2_HOME}/settings.xml]"
+
+cat > "${M2_HOME}/settings.xml" <<EOF
+
+<?xml version="1.0" encoding="UTF-8"?>
+<settings>
+	<servers>
+		<server>
+			<id>\${M2_SETTINGS_REPO_ID}</id>
+			<username>\${M2_SETTINGS_REPO_USERNAME}</username>
+			<password>\${M2_SETTINGS_REPO_PASSWORD}</password>
+		</server>
+	</servers>
+</settings>
+
+
+EOF
+echo "Settings xml written"
+
 
 cd ../../../code-repo
 
