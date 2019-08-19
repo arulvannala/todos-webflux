c399f42548afaa2ce71867713da362707bf933a2
Dodd Pfeffer <dpfeffer@pivotal.io>
2019-08-18 20:57:48 -0400

    Updates CI scripts

--- ci/tasks/prepare-cf-push.sh
+++ ci/tasks/prepare-cf-push.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
-version=`cat version/version`
-artifactName="${ARTIFACT_ID}-${version}.jar"
-
-cd deployment
-
-export CALCULATED_GROUP_ID=${GROUP_ID//\./\/}
-ARTIFACT_FULL_URL="${ARTIFACTORY_URL}/${GROUP_ID//\.//}/${ARTIFACT_ID}/${VERSION}/${ARTIFACT_NAME}"
+#version=`cat version/version`
+#artifactName="${ARTIFACT_ID}-${version}.jar"
+#
+#cd deployment
+#
+#export CALCULATED_GROUP_ID=${GROUP_ID//\./\/}
+#ARTIFACT_FULL_URL="${ARTIFACTORY_URL}/${GROUP_ID//\.//}/${ARTIFACT_ID}/${VERSION}/${ARTIFACT_NAME}"
 echo "Downloading full artifact url: ${ARTIFACT_FULL_URI}"
-curl "${ARTIFACT_FULL_URI}" -O
-
-cp ../code-repo/ci-manifest.yml manifest.yml
-
-sed -i -- "s|path: .*$|path: $artifactName|g" manifest.yml
-sed -i -- "s|- sampleroute|- ${ROUTE}|g" manifest.yml
-
-cat manifest.yml
+#curl "${ARTIFACT_FULL_URI}" -O
+#
+#cp ../code-repo/ci-manifest.yml manifest.yml
+#
+#sed -i -- "s|path: .*$|path: $artifactName|g" manifest.yml
+#sed -i -- "s|- sampleroute|- ${ROUTE}|g" manifest.yml
+#
+#cat manifest.yml
